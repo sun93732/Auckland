d4e2d1cc24263dd1f84779e5d65205fb8c757f13
sun93732 <sunshuaister@gmail.com>
2015-02-23 09:20:47 +0800

    how to remove metadata

--- TestMart/src/TestDirectClientWithoutImport.java
+++ TestMart/src/TestDirectClientWithoutImport.java
@@ -9,14 +9,12 @@ import org.oracle.ProductCatalogInterface;
 public class TestDirectClientWithoutImport {
 	public static void main(String[] args) throws Exception {
 
-		URL url = new URL(
-				"http://localhost:8080/TestMart/TestMartCatalogService?wsdl");
-
+//		URL url = new URL("http://localhost:8080/TestMart/TestMartCatalogService?wsdl");
+		URL url = new URL("http://localhost:8888/productCatalogB?wsdl");
 		// 1st argument service URI, refer to wsdl document above
 		// 2nd argument is service name, refer to wsdl document above
-		QName qname = new QName("http://www.oracle.com",
-				"TestMartCatalogService");
-
+//		QName qname = new QName("http://www.oracle.com","TestMartCatalogService");
+		QName qname = new QName("http://www.oracle.com","TestMartCatalog");
 		Service service = Service.create(url, qname);
 
 		ProductCatalogInterface hello = (ProductCatalogInterface) service.getPort(ProductCatalogInterface.class);
