44cdbc11e8e26319c847b5e838b52425cf49ea6b
sun93732 <sunshuaister@gmail.com>
2015-02-25 21:51:59 +0800

    Spring applicationcontextAware and BeanNameAware

--- SpringDemo/src/com/eric/spring/Triangle.java
+++ SpringDemo/src/com/eric/spring/Triangle.java
@@ -1,9 +1,16 @@
 package com.eric.spring;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware{
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private String beanName;
+    private ApplicationContext ctx = null;
 	
 	public Point getPointA() {
 		return pointA;
@@ -33,5 +40,21 @@ public class Triangle {
 		System.out.println("PointA: " + this.getPointA());
 		System.out.println("PointB:" + this.getPointB());
 		System.out.println("PointC:" + this.getPointC());
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext ctx)
+			throws BeansException {
+		// TODO Auto-generated method stub
+		this.ctx = ctx;
+		
+	}
+
+	@Override
+	public void setBeanName(String arg0) {
+		// TODO Auto-generated method stub
+		this.beanName = arg0;
+		System.out.println("Bean name is " +  beanName);
+		
 	}	
 }
